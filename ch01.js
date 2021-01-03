92b547a1ce81c095731d56ea5cf8d6a8685a91f6
carina1754 <bulebed1754@gmail.com>
2021-01-04 01:21:22 +0900

    2021-01-04-01:21

--- ch01.js
+++ ch01.js
@@ -1,1 +1,8 @@
-console.log('123');
+var http = require('http');
+
+http.createServer(function(request,response){
+  response.writeHead(200,{'Content-Type':'text/html'});
+  response.end('<h1>hell</h1>');
+}).listen(3333,function(){
+  console.log("run");
+})
